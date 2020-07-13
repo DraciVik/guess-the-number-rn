94f7971f1acda9f91a1c4a1d7d21517ecee69029
Viktor Stojanov <viktor.stojanov90@gmail.com>
2020-07-13 11:24:37 +0200

    Added StartGameScreen and Card component as well as Constants folder where I'll keep all the constants

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import Header from "./components/Header";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import StartGameScreen from './screens/StartGameScreen';
+import Header from './components/Header';
 
 export default function App() {
-	return (
-		<View style={styles.screen}>
-			<Header title="Guess a number" />
-		</View>
-	);
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a number" />
+      <StartGameScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-	},
+  screen: {
+    flex: 1,
+  },
 });
